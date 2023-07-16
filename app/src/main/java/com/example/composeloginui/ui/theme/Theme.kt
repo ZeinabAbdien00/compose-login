548323e1e1d6a44710f67a037601c6fb07c88f04
Zeinab Abdien <suzanabdien15@gmail.com>
2023-07-17 00:43:38 +0300

    add needed themes

--- app/src/main/java/com/example/composeloginui/ui/theme/Theme.kt
+++ app/src/main/java/com/example/composeloginui/ui/theme/Theme.kt
@@ -2,44 +2,25 @@ package com.example.composeloginui.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
-
-private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
-)
+import androidx.compose.ui.graphics.Color
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
-    onPrimary = Color.White,
-    onSecondary = Color.Black,
-    onBackground = Color.Black,
-    onSurface = Color.Black,
-    */
+    primary = LightBlue800,
+    primaryVariant = LightBlue500,
+    secondary = LightBlue900,
+    background = Color.DarkGray,
 )
 
 @Composable
-fun ComposeLoginUiTheme(
+fun FancyLoginScreenTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
 
     MaterialTheme(
-        colors = colors,
+        colors = LightColorPalette,
         typography = Typography,
         shapes = Shapes,
         content = content
